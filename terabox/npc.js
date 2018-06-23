29c86b3c1b9c4add98a46044fc66fde70e8af013
ACoolZero <nphicuong.is@gmail.com>
2018-06-23 14:21:49 +0700

    Update npc.js

--- terabox/npc.js
+++ terabox/npc.js
@@ -1,6 +1,7 @@
 function footer(){
-    $('.footer-body').html('<img id="terabox-logo" src="https://www.terabox.vn/wp-content/uploads/2017/11/logo-Terabox.jpg" />');
-    alert('4');
+    //$('.footer-body').html('<img id="terabox-logo" src="https://www.terabox.vn/wp-content/uploads/2017/11/logo-Terabox.jpg" />');
+    document.getElementById("footer").innerHTML='<img id="terabox-logo" src="https://www.terabox.vn/wp-content/uploads/2017/11/logo-Terabox.jpg" />';
+    alert('5');
 }
 
 footer();
