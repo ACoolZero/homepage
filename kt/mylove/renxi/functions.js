d83cfaaf1f6bd98f34f54511aba52478d3919062
ACoolZero <nphicuong.is@gmail.com>
2018-08-13 03:52:01 +0700

    Update functions.js

--- kt/mylove/renxi/functions.js
+++ kt/mylove/renxi/functions.js
@@ -12,28 +12,25 @@ $(window).resize(function() {
 });
 
 (function($) {
-	$.fn.typeSSwriter = function(stoped = false) {
-		var thisTimer = setInterval(function() {
-			if (stoped) {
-				clearInterval(thisTimer);
-			}
-			this.each(function() {
-				var $ele = $(this), str = $ele.html(), progress = 0;
-				$ele.html('');
-				var timer = setInterval(function() {
-					var current = str.substr(progress, 1);
-					if (current == '<') {
-						progress = str.indexOf('>', progress) + 1;
-					} else {
-						progress++;
-					}
-					$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
-					if (progress >= str.length-1) {
-						clearInterval(timer);
-					}
-				}, 75);
-			})
-		}, 75);
+	$.fn.typewriter = function(stoped = false) {
+		var thisTimer = this.each(function() {
+			var $ele = $(this), str = $ele.html(), progress = 0;
+			$ele.html('');
+			var timer = setInterval(function() {
+				if (stoped)
+					$(this).stop(true,true);
+				var current = str.substr(progress, 1);
+				if (current == '<') {
+					progress = str.indexOf('>', progress) + 1;
+				} else {
+					progress++;
+				}
+				$ele.html(str.substring(0, progress) + (progress & 1 ? '_' : ''));
+				if (progress >= str.length-1) {
+					clearInterval(timer);
+				}
+			}, 75);
+		});
 		return this;
 	};
 })(jQuery);
