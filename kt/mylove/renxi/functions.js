9dc3a15316921f04da1cfd7e55dddbd302706405
ACoolZero <nphicuong.is@gmail.com>
2018-08-13 18:10:39 +0700

    Update functions.js

--- kt/mylove/renxi/functions.js
+++ kt/mylove/renxi/functions.js
@@ -13,7 +13,7 @@ $(window).resize(function() {
 
 (function($) {
 	$.fn.typewriter = function() {
-		event.stopPropagation();
+		//event.stopPropagation();
 		this.each(function() {
 			var $ele = $(this), str = $ele.html(), progress = 0;
 			$ele.html('');
